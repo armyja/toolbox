369176661ef5a9e0ce4c1aeb4dee6ca48cdc2252
wangjiahui 30036925 <wangjiahui36@huawei.com>
2024-02-28 16:05:43 +0800

    add select date

--- src/routes/event-recorder/+page.svelte
+++ src/routes/event-recorder/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { onMount, tick } from 'svelte';
-	import { format, intervalToDuration, type Duration, formatDuration } from 'date-fns';
+	import { format, intervalToDuration, type Duration, startOfDay, endOfDay } from 'date-fns';
 	import { openDB, type DBSchema, type IDBPDatabase, type StoreValue } from 'idb';
+	// 初始化变量
+	let selected_date = new Date();
 	// Database
 	interface MyDB extends DBSchema {
 		tags: {
@@ -47,11 +49,14 @@
 		await db.put('events', val);
 	}
 	async function init() {
-  if (database === (void 0)) {
-    database = await useDB();
-    await tick();
-  }
-		fetch_data();
+		if (database === void 0) {
+			database = await useDB();
+			await tick();
+		}
+		notify();
+	}
+	async function notify() {
+		fetch_data(selected_date);
 	}
 	// 插入数据
 	let current_date: Date | undefined;
@@ -74,7 +79,7 @@
 		current_date = undefined;
 		text = '';
 		note = '';
-		fetch_data();
+		notify();
 	}
 	function update_now_if_not_exist() {
 		if (current_date === void 0) {
@@ -86,17 +91,11 @@
 		duration?: Duration;
 	}
 	let sRecords: Sevent[] = [];
-	let records: StoreValue<MyDB, 'events'>[] = [];
-	async function fetch_data() {
-		let zeroDate = new Date();
-		zeroDate.setHours(0);
-		zeroDate.setMinutes(0);
-		zeroDate.setMilliseconds(0);
-		zeroDate.setSeconds(0);
+	async function fetch_data(aDate: Date) {
 		let records: StoreValue<MyDB, 'events'>[] = await database.getAllFromIndex(
 			'events',
 			'by-date',
-			IDBKeyRange.lowerBound(zeroDate)
+			IDBKeyRange.bound(startOfDay(aDate), endOfDay(aDate))
 		);
 		let m_sRecords: Sevent[] = [];
 		let map = new Map<string, Date>();
@@ -115,6 +114,20 @@
 		}
 		sRecords = m_sRecords;
 	}
+	async function find_date(is_after: boolean) {
+		let bound_date = is_after ? endOfDay(selected_date) : startOfDay(selected_date);
+		let range = is_after
+			? IDBKeyRange.lowerBound(bound_date, true)
+			: IDBKeyRange.upperBound(bound_date, true);
+		let keys = await database.getAllKeysFromIndex('events', 'by-date', range);
+		console.log("keys", keys)
+		let index = is_after ? 0 : keys.length - 1;
+		if (keys.length == 0) {
+			return;
+		}
+		selected_date = (await database.get('events', keys[index]))?.date || new Date();
+		notify()
+	}
 	let last_click = new Date();
 	async function delete_events() {
 		let d = new Date();
@@ -124,16 +137,16 @@
 		}
 		last_click = new Date();
 		await Promise.allSettled(sRecords.map((e) => database.delete('events', e.id || 0)));
-		fetch_data();
+		notify();
 	}
 	async function delete_event(id: number) {
 		await database.delete('events', id);
-		fetch_data();
+		notify();
 	}
 	function toZhCnDuration(duration: Duration) {
-		let hStr = duration.hours ? duration.hours + ' ' + '时' : ''
-		let mStr = duration.minutes ? duration.minutes + ' ' + '分' : ''
-		let sStr = duration.seconds ? duration.seconds + ' ' + '秒' : ''
+		let hStr = duration.hours ? duration.hours + ' ' + '时' : '';
+		let mStr = duration.minutes ? duration.minutes + ' ' + '分' : '';
+		let sStr = duration.seconds ? duration.seconds + ' ' + '秒' : '';
 		return [hStr, mStr, sStr].join(' ');
 	}
 	onMount(() => {
@@ -149,9 +162,7 @@
 <div class="text-column">
 	<h1 class="text-xl">事件记录</h1>
 
-	<p>
-		自动生成时间戳；生成当日记录汇总。筛选日期；导出日志；清除事件。
-	</p>
+	<p>自动生成时间戳；生成当日记录汇总。筛选日期；导出日志；清除事件。</p>
 
 	<div class="flex justify-between">
 		<div class="flex-none w-18 py-2">
@@ -178,14 +189,24 @@
 			<button on:click={save_event}>S</button>
 		</div>
 	</div>
-	<div>{format(new Date(), 'yyyy-MM-dd')} <button on:click={delete_events}>D</button></div>
+	<div class="flex justify-between">
+		<div class="flex items-center">
+			<button on:click={() => find_date(false)}>&lt;</button>
+			<p class="mx-1 inline-block align-middle">{format(selected_date, 'yyyy-MM-dd')}</p>
+			<button
+				on:click={() => find_date(true)}
+				disabled={new Date().toDateString() === selected_date.toDateString()}>&gt;</button
+			>
+		</div>
+		<button on:click={delete_events}>X</button>
+	</div>
 	<ul>
 		{#each sRecords as record}
 			<li class="py-1">
-				<span>{format(record.date, 'HH:mm:ss')}</span> - <span>{record.tag}</span>
+				<span class="font-mono">{format(record.date, 'HH:mm:ss')}</span> - <span>{record.tag}</span>
 				<span>{record.note ? '| ' + record.note : ''}</span>
 				<span>{record.duration ? '| ' + toZhCnDuration(record.duration || {}) : ''}</span>
-				<button on:click={delete_event(record.id || 0)}>D</button>
+				<button on:click={() => delete_event(record.id || 0)}>X</button>
 			</li>
 		{/each}
 	</ul>
@@ -193,6 +214,6 @@
 
 <style lang="postcss">
 	button {
-		@apply rounded-md bg-indigo-600 px-2 py-1 text-sm font-semibold text-white select-none;
+		@apply rounded-md bg-slate-500 px-2 py-1 text-sm font-semibold text-white select-none disabled:bg-slate-400;
 	}
 </style>
